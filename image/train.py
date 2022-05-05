94ea93bc864503100637a9b1f7fc3dffbc4b9ae2
Harsh Raj <harsh777111raj@gmail.com>
2022-05-06 02:09:02 +0530

    commit on 'main'

--- image/train.py
+++ image/train.py
@@ -1,148 +1,141 @@
 import torch
-from abc import abstractmethod
-from numpy import inf
-from logger import TensorboardWriter
 
 
-class Trainer:
-    """
-    Trainer class
-    """
-    def __init__(self, model, criterion, metric_ftns, optimizer, config):
-        self.config = config
-        self.logger = config.get_logger('trainer', config['trainer']['verbosity'])
+class Trainer():
+    def __init__(self,teacher, 
+                      student,
+                      discriminator,
+                      optimizer_S,
+                      optimizer_D,
+                      loss,
+                      save_dir,
+                      ckpt_path,):
+        super(Trainer, self).__init__()
+        self.teacher = teacher
+        self.student = student
+        self.discriminator = discriminator
+        self.optimizer_S = optimizer_S
+        self.optimizer_D = optimizer_D
+        self.loss = loss
+        self.save_dir = save_dir
+        self.ckpt_path = ckpt_path
 
-        self.model = model
-        self.criterion = criterion
-        self.metric_ftns = metric_ftns
-        self.optimizer = optimizer
+    def train(dataloader_T, dataloader_S):
+        self.student.train()
+        self.discriminator.train()
 
-        cfg_trainer = config['trainer']
-        self.epochs = cfg_trainer['epochs']
-        self.save_period = cfg_trainer['save_period']
-        self.monitor = cfg_trainer.get('monitor', 'off')
+        Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        for i, (data_T, data_S) in enumerate(zip(cycle(dataloader_T), dataloader_S)):
+            data_T, data_S = data_T.to(device), data_S.to(device)
 
-        # configuration to monitor model performance and save best
-        if self.monitor == 'off':
-            self.mnt_mode = 'off'
-            self.mnt_best = 0
-        else:
-            self.mnt_mode, self.mnt_metric = self.monitor.split()
-            assert self.mnt_mode in ['min', 'max']
+            # Adversarial ground truths
+            valid = Variable(Tensor(data_T.size(0), 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(data_T.size(0), 1).fill_(0.0), requires_grad=False)
 
-            self.mnt_best = inf if self.mnt_mode == 'min' else -inf
-            self.early_stop = cfg_trainer.get('early_stop', inf)
-            if self.early_stop <= 0:
-                self.early_stop = inf
+            with torch.no_grad:
+                real_emb = self.teacher(data_T)
 
-        self.start_epoch = 1
+            # -----------------
+            #  Train Student
+            # -----------------
+            optimizer_S.zero_grad()
 
-        self.checkpoint_dir = config.save_dir
+            # Generate a batch of images
+            gen_emb = self.student(data_S)
 
-        # setup visualization writer instance                
-        self.writer = TensorboardWriter(config.log_dir, self.logger, cfg_trainer['tensorboard'])
+            # Loss measures Student's ability to fool the discriminator
+            g_loss = loss(discriminator(gen_emb), valid)
 
-        if config.resume is not None:
-            self._resume_checkpoint(config.resume)
+            g_loss.backward()
+            optimizer_S.step()
 
-    @abstractmethod
-    def _train_epoch(self, epoch):
-        """
-        Training logic for an epoch
-        :param epoch: Current epoch number
-        """
-        raise NotImplementedError
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            optimizer_D.zero_grad()
 
-    def train(self):
-        """
-        Full training logic
-        """
-        not_improved_count = 0
-        for epoch in range(self.start_epoch, self.epochs + 1):
-            result = self._train_epoch(epoch)
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = loss(self.discriminator(real_emb), valid)
+            fake_loss = loss(self.discriminator(gen_emb.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
 
-            # save logged informations into log dict
-            log = {'epoch': epoch}
-            log.update(result)
+            d_loss.backward()
+            optimizer_D.step()
 
-            # print logged informations to the screen
-            for key, value in log.items():
-                self.logger.info('    {:15s}: {}'.format(str(key), value))
+            print(
+                "[TRAIN][Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            )
 
-            # evaluate model performance according to configured metric, save best checkpoint as model_best
-            best = False
-            if self.mnt_mode != 'off':
-                try:
-                    # check whether model performance improved or not, according to specified metric(mnt_metric)
-                    improved = (self.mnt_mode == 'min' and log[self.mnt_metric] <= self.mnt_best) or \
-                               (self.mnt_mode == 'max' and log[self.mnt_metric] >= self.mnt_best)
-                except KeyError:
-                    self.logger.warning("Warning: Metric '{}' is not found. "
-                                        "Model performance monitoring is disabled.".format(self.mnt_metric))
-                    self.mnt_mode = 'off'
-                    improved = False
+    def val(dataloader_T, dataloader_S):
+        self.student.eval()
+        self.discriminator.eval()
 
-                if improved:
-                    self.mnt_best = log[self.mnt_metric]
-                    not_improved_count = 0
-                    best = True
-                else:
-                    not_improved_count += 1
+        Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        for i, (data_T, data_S) in enumerate(zip(cycle(self.dataloader_T), self.dataloader_S)):
+            data_T, data_S = data_T.to(device), data_S.to(device)
 
-                if not_improved_count > self.early_stop:
-                    self.logger.info("Validation performance didn\'t improve for {} epochs. "
-                                     "Training stops.".format(self.early_stop))
-                    break
+            # Adversarial ground truths
+            valid = Variable(Tensor(data_T.size(0), 1).fill_(1.0), requires_grad=False)
+            fake = Variable(Tensor(data_T.size(0), 1).fill_(0.0), requires_grad=False)
 
-            if epoch % self.save_period == 0:
-                self._save_checkpoint(epoch, save_best=best)
+            with torch.no_grad:
+                real_emb = self.teacher(data_T)
 
-    def _save_checkpoint(self, epoch, save_best=False):
-        """
-        Saving checkpoints
-        :param epoch: current epoch number
-        :param log: logging information of the epoch
-        :param save_best: if True, rename the saved checkpoint to 'model_best.pth'
-        """
-        arch = type(self.model).__name__
-        state = {
-            'arch': arch,
-            'epoch': epoch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-            'monitor_best': self.mnt_best,
-            'config': self.config
-        }
-        filename = str(self.checkpoint_dir / 'checkpoint-epoch{}.pth'.format(epoch))
+            # -----------------
+            #  Train Student
+            # -----------------
+            # Generate a batch of data
+            gen_emb = self.student(data_S)
+
+            # Loss measures Student's ability to fool the discriminator
+            g_loss = loss(self.discriminator(gen_emb), valid)
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+            optimizer_D.zero_grad()
+
+            # Measure discriminator's ability to classify real from generated samples
+            real_loss = loss(self.discriminator(real_emb), valid)
+            fake_loss = loss(self.discriminator(gen_emb.detach()), fake)
+            d_loss = (real_loss + fake_loss) / 2
+
+            print(
+                "[VAL][Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, n_epochs, i, len(valloader), d_loss.item(), g_loss.item())
+            )
+
+    def load_checkpoint(student, discriminator,
+                        optimizer_S, optimizer_D, 
+                        ckpt_path):
+        checkpoint = torch.load(ckpt_path)
+        student.load_state_dict(checkpoint['student_dict'])
+        discriminator.load_state_dict(checkpoint['discriminator_dict'])
+        optimizer_S.load_state_dict(checkpoint['optimizer_S_dict'])
+        optimizer_D.load_state_dict(checkpoint['optimizer_D_dict'])
+        return student, discriminator, optimizer_S, optimizer_D
+
+    def save_checkpoint(state, filename):
         torch.save(state, filename)
-        self.logger.info("Saving checkpoint: {} ...".format(filename))
-        if save_best:
-            best_path = str(self.checkpoint_dir / 'model_best.pth')
-            torch.save(state, best_path)
-            self.logger.info("Saving current best: model_best.pth ...")
 
-    def _resume_checkpoint(self, resume_path):
-        """
-        Resume from saved checkpoints
-        :param resume_path: Checkpoint path to be resumed
-        """
-        resume_path = str(resume_path)
-        self.logger.info("Loading checkpoint: {} ...".format(resume_path))
-        checkpoint = torch.load(resume_path)
-        self.start_epoch = checkpoint['epoch'] + 1
-        self.mnt_best = checkpoint['monitor_best']
+    def fit(n_epochs,
+            trainloader_T, trainloader_S,
+            valloader_T, valloader_S,):
+        
+        if self.ckpt_path:
+            self.student, self.discriminator, self.optimizer_S, self.optimizer_D =\
+                load_checkpoint(self.student, self.discriminator,
+                                self.optimizer_S, self.optimizer_D, 
+                                self.ckpt_path)
+                                
+        for epoch in range(n_epochs):
+            self.train(trainloader_T, trainloader_S)
+            self.val(valloader_T, valloader_S)
 
-        # load architecture params from checkpoint.
-        if checkpoint['config']['arch'] != self.config['arch']:
-            self.logger.warning("Warning: Architecture configuration given in config file is different from that of "
-                                "checkpoint. This may yield an exception while state_dict is being loaded.")
-        self.model.load_state_dict(checkpoint['state_dict'])
-
-        # load optimizer state from checkpoint only when optimizer type is not changed.
-        if checkpoint['config']['optimizer']['type'] != self.config['optimizer']['type']:
-            self.logger.warning("Warning: Optimizer type given in config file is different from that of checkpoint. "
-                                "Optimizer parameters not being resumed.")
-        else:
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
-
-        self.logger.info("Checkpoint loaded. Resume training from epoch {}".format(self.start_epoch))
+            self.save_checkpoint({
+            'student_dict': self.student.state_dict(),
+            'discriminator_dict': self.discriminator.state_dict(),
+            'optimizer_S_dict': self.optimizer_S.state_dict(),
+            'optimizer_D_dict': self.optimizer_D.state_dict(),
+            }, filename=save_dir)
